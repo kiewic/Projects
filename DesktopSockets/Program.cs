e2739ec87141c2de7929c0ec7aac67a235f34605
kiewic <kiewic@gmail.com>
2012-10-13 19:18:16 -0700

    Initial commit.

---
+++ DesktopSockets/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                PrintHelp();
+            }
+            else if (args[0] == "tcp" && args[1] == "server")
+            {
+                DoTcpServer();
+            }
+            else if (args[0] == "tcp" && args[1] == "client")
+            {
+                DoTcpClient();
+            }
+            else if (args[0] == "udp" && args[1] == "receive")
+            {
+                DoUdpReceive();
+            }
+            else if (args[0] == "udp" && args[1] == "send")
+            {
+                DoUdpSend();
+            }
+            else
+            {
+                PrintHelp();
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: {0} tcp {{ server | client }}", Environment.GetCommandLineArgs()[0]);
+            Console.WriteLine("Usage: {0} udp {{ send | receive }}", Environment.GetCommandLineArgs()[0]);
+        }
+
+        private static void DoTcpServer()
+        {
+            Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            // Is socket is already taken, "SocketException: Only one usage of each socket address (protocol/network
+            // address/port) is normally permitted (-2147467259)" is thrown.
+            socket.Bind(new IPEndPoint(IPAddress.IPv6Any, 80));
+            socket.Listen(5);
+            while (true)
+            {
+                using (Socket client = socket.Accept())
+                {
+                    // We cna improve this making it multithreading.
+                    OnConnectionReceived(client);
+                }
+            }
+        }
+
+        private static void OnConnectionReceived(Socket client)
+        {
+            try
+            {
+                Console.WriteLine("{0} connected.", client.RemoteEndPoint);
+                NetworkStream networkStream = new NetworkStream(client);
+
+                while (true)
+                {
+                    // Read bytes in multiples of 16 to make it more fun.
+                    byte[] buffer = new byte[16];
+                    string request = "";
+                    while (!request.EndsWith("\r\n"))
+                    {
+                        int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                        if (bytesRead == 0)
+                        {
+                            // If bytesRead is zero, incoming stream was closed.
+                            Console.WriteLine("{0} incoming stream closed.", client.RemoteEndPoint);
+                            return;
+                        }
+                        request += Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    }
+                    Console.WriteLine(request);
+
+                    string response = "Yes, I am ñoño. The time is " + DateTime.Now + ".\r\n";
+                    buffer = Encoding.UTF8.GetBytes(response);
+
+                    // If Write() is called multiple times, response may be sent in multiple TCP packets.
+                    networkStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            //catch (IOException)
+            finally
+            {
+                Console.WriteLine("{0} disconnected.", client.RemoteEndPoint);
+            }
+        }
+
+        private static void DoTcpClient()
+        {
+            using (Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp))
+            {
+                ConnectSocket(socket, "localhost", 80);
+
+                NetworkStream networkStream = new NetworkStream(socket);
+
+                string request = "Are you noño? Can you tell me the time?\r\n";
+                byte[] buffer = Encoding.UTF8.GetBytes(request);
+                networkStream.Write(buffer, 0, buffer.Length);
+
+                // Read bytes in multiples of 16 to make it more fun.
+                buffer = new byte[16];
+                string response = "";
+                while (!response.EndsWith("\r\n"))
+                {
+                    int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        // If bytesRead is zero, incoming stream was closed.
+                        Console.WriteLine("Incoming stream from {0} closed.", socket.RemoteEndPoint);
+                        return;
+                    }
+                    response += Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                }
+                Console.WriteLine(response);
+
+                Console.WriteLine("Disconnecting from {0}.", socket.RemoteEndPoint);
+            }
+        }
+
+        private static void ConnectSocket(Socket socket, string hostname, int port)
+        {
+            // If host is unknown, "SocketException: No such host is known (-2147467259)" is thrown.
+            IPHostEntry host = Dns.GetHostEntry(hostname);
+
+            for (int i = 0; i < host.AddressList.Length; i++)
+            {
+                try
+                {
+                    socket.Connect(host.AddressList[i], port);
+                    Console.WriteLine("Connected to {0}.", socket.RemoteEndPoint);
+                    break;
+                }
+                catch (SocketException ex)
+                {
+                    // If remote host does not reply, "SocketException: No connection could be made because the target
+                    // machine actively  refused it (-2147467259)" is thrown.
+                    if (ex.HResult == -2147467259 && i + 1 != host.AddressList.Length)
+                    {
+                        continue;
+                    }
+                    throw;
+                }
+            }
+        }
+
+        private static void DoUdpReceive()
+        {
+            using (Socket udpSocket = new Socket(SocketType.Dgram, ProtocolType.Udp))
+            {
+                // If port is already taken, "SocketException: Only one usage of each socket address (protocol/network
+                // address/port) is normally permitted (-2147467259)" is thrown.
+                udpSocket.Bind(new IPEndPoint(IPAddress.IPv6Any, 2704));
+
+                while (true)
+                {
+                    // In theory, 1500 is the maximum size of a datagram.
+                    byte[] buffer = new byte[1500];
+                    EndPoint remoteEndPoint = new IPEndPoint(IPAddress.IPv6Any, 0);
+                    int bytesReceived = udpSocket.ReceiveFrom(buffer, ref remoteEndPoint);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+                    Console.WriteLine("Message received from {0}: {1}", remoteEndPoint, message);
+                }
+            }
+        }
+
+        private static void DoUdpSend()
+        {
+            using (Socket udpSocket = new Socket(SocketType.Dgram, ProtocolType.Udp))
+            {
+                string message = "¡Hello, I am the new guy in the network!";
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse("::1"), 2704);
+                int bytesSent = udpSocket.SendTo(buffer, remoteEndPoint);
+                Console.WriteLine("Message sent to {0}: {1}", remoteEndPoint, message);
+            }
+        }
+
+    }
+}
---
+++ SocketsSockets/MainPage.xaml.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SocketsSockets
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void DisplayOutput(TextBlock textBlock, string message)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => textBlock.Text = message);
+        }
+
+        //
+        // TCP server.
+        //
+        StreamSocketListener listener = null;
+
+        private async void StartServer_Click_1(object sender, RoutedEventArgs e)
+        {
+            listener = new StreamSocketListener();
+
+            // ConnectionReceived handler must be set before BindServiceNameAsync is called, if not
+            // "A method was called at an unexpected time. (Exception from HRESULT: 0x8000000E)" error occurs.
+            listener.ConnectionReceived += OnConnectionReceived;
+
+            // Trying to bind more than once to the same port throws "Only one usage of each socket address (protocol/
+            // network address/port) is normally permitted. (Exception from HRESULT: 0x80072740)" exception.
+            await listener.BindServiceNameAsync("80");
+            DisplayOutput(TcpServerOutput, "Listening.");
+        }
+
+        private async void OnConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            try
+            {
+                DataReader reader = new DataReader(args.Socket.InputStream);
+                reader.InputStreamOptions = InputStreamOptions.Partial;
+                DataWriter writer = new DataWriter(args.Socket.OutputStream);
+
+                DisplayOutput(TcpServerOutput,args.Socket.Information.RemoteAddress.DisplayName +
+                    " connected.");
+
+                while (true)
+                {
+                    // Read request.
+                    string request = "";
+                    while (!request.EndsWith("\r\n"))
+                    {
+                        // Read bytes in multiples of 16 to make it more fun.
+                        uint bytesRead = await reader.LoadAsync(16);
+                        if (bytesRead == 0)
+                        {
+                            // If bytesRead is zero, incoming stream was closed.
+                            DisplayOutput(TcpServerOutput, "The client is gone.");
+                            return;
+                        }
+                        request += reader.ReadString(bytesRead);
+                    }
+                    DisplayOutput(TcpServerOutput, request);
+
+                    // Send response.
+                    string response = "Yes, I am ñoño. The time is " + DateTime.Now + ".\r\n";
+
+                    // This is useless in this sample.
+                    uint responseLength = writer.MeasureString(response);
+
+                    writer.WriteString(response);
+                    uint bytesWritten = await writer.StoreAsync();
+
+                    Debug.Assert(bytesWritten == responseLength);
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayOutput(TcpServerOutput, ex.ToString());
+            }
+        }
+
+        private void StopServer_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (listener != null)
+            {
+                listener.Dispose();
+                listener = null;
+                DisplayOutput(TcpServerOutput, "Not listening anymore.");
+            }
+        }
+
+        //
+        // TCP client.
+        //
+        private StreamSocket socket = null;
+        private Uri uri = null;
+
+        private async void ConnectClient_Click_1(object sender, RoutedEventArgs e)
+        {
+            socket = new StreamSocket();
+            uri = new Uri("http://localhost/?foo=bar");
+
+            await socket.ConnectAsync(new HostName(uri.Host), "80");
+            DisplayOutput(TcpClientOutput, "Connected.");
+        }
+
+        private async void SendRequest_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (socket != null)
+            {
+                DataReader reader = new DataReader(socket.InputStream);
+                reader.InputStreamOptions = InputStreamOptions.Partial;
+                DataWriter writer = new DataWriter(socket.OutputStream);
+
+                string request = "Are you noño? Can you tell me the time?\r\n";
+
+                writer.WriteString(request);
+                uint bytesWritten = await writer.StoreAsync();
+
+                // Why DataReader doesn't have ReadChar()?
+                string response = "";
+                while (!response.EndsWith("\r\n"))
+                {
+                    // Read bytes in multiples of 16, to make it more fun.
+                    // If the socket is closed while we are reading, the "The I/O operation has been aborted because
+                    // of either a thread exit or an application request. (Exception from HRESULT: 0x800703E3)"
+                    // exception is thrown.
+                    uint bytesRead = await reader.LoadAsync(16);
+                    if (bytesRead == 0)
+                    {
+                        // If bytesRead is zero, incoming stream was closed.
+                        DisplayOutput(TcpClientOutput, "The server is gone.");
+                        return;
+                    }
+                    response += reader.ReadString(bytesRead);
+                }
+                DisplayOutput(TcpClientOutput, response);
+
+                // Do not use Dispose(). If used, streams cannot be used anymore.
+                //reader.Dispose();
+                //writer.Dispose();
+
+                // I think this is needed, because without this, the DataReader destructor closes the stream,
+                // and closing the stream might set the FIN control bit.
+                reader.DetachStream();
+                writer.DetachStream();
+            }
+        }
+
+        private void DisconnectClient_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (socket != null)
+            {
+                socket.Dispose();
+                socket = null;
+                DisplayOutput(TcpClientOutput, "Closed.");
+            }
+        }
+
+        //
+        // UDP receive.
+        //
+
+        // Make it global, if not, socket goes out of scope after first message is received.
+        DatagramSocket receiveSocket = null;
+
+        private async void ConnectUdpReceive_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (receiveSocket == null)
+            {
+                receiveSocket = new DatagramSocket();
+
+                // MessageReceived handler must be set before BindServiceAsync is called, if not
+                // "A method was called at an unexpected time. (Exception from HRESULT: 0x8000000E)" error occurs.
+                receiveSocket.MessageReceived += OnMessageReceived;
+
+                await receiveSocket.BindServiceNameAsync("2704");
+
+                DisplayOutput(UdpReceiveOutput, "Connected (bound).");
+            }
+        }
+
+        private void DisconnectUdpReceive_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (receiveSocket != null)
+            {
+                receiveSocket.Dispose();
+                receiveSocket = null;
+                DisplayOutput(UdpReceiveOutput, "Disconnected.");
+            }
+        }
+
+        private void OnMessageReceived(DatagramSocket sender, DatagramSocketMessageReceivedEventArgs args)
+        {
+            DataReader reader = args.GetDataReader();
+            reader.InputStreamOptions = InputStreamOptions.Partial;
+            // LoadAsync not needed. The reader comes already loaded.
+            uint bytesRead = reader.UnconsumedBufferLength;
+            string message = reader.ReadString(bytesRead);
+
+            DisplayOutput(UdpReceiveOutput, message);
+        }
+
+        //
+        // UDP send.
+        //
+
+        private async void UdpSend_Click_1(object sender, RoutedEventArgs e)
+        {
+            DatagramSocket socket = new DatagramSocket();
+
+            // Even when we do not except any response, this handler is called if any error occurrs.
+            socket.MessageReceived += socket_MessageReceived;
+
+            // DatagramSocket.ConnectAsync() vs datagramSocket.GetOutputStreamAsync()?
+            await socket.ConnectAsync(new HostName("localhost"), "2704");
+
+            DataWriter writer = new DataWriter(socket.OutputStream);
+            string message = "¡Hello, I am the new guy in the network!";
+            writer.WriteString(message);
+
+            // If GetOutputStreamAsync was used instead of ConnectAsync, this throws
+            // "An existing connection was forcibly closed by the remote host. (Exception from HRESULT: 0x80072746)"
+            // exepction.
+            uint bytesWritten = await writer.StoreAsync();
+
+            DisplayOutput(UdpSendOutput, "Message sent.");
+        }
+
+        private void socket_MessageReceived(DatagramSocket sender, DatagramSocketMessageReceivedEventArgs args)
+        {
+            try
+            {
+                // If remote peer didn't received message, exception {"An existing connection was forcibly closed by the
+                // remote host. (Exception from HRESULT: 0x80072746)" is thrown.
+                uint bytesRead = args.GetDataReader().UnconsumedBufferLength;
+            }
+            catch (Exception)
+            {
+                DisplayOutput(UdpSendOutput, "Peer didn't receive message.");
+            }
+        }
+
+    }
+}
